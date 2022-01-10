7d94b86b61a86065c2013665b999622ed119ce61
ana-str <anastrugar20@gmail.com>
2022-01-10 17:38:56 +0200

    one

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/model/Unternehmensgröße.java
@@ -0,0 +1,5 @@
+package model;
+
+public class Unternehmensgröße {
+    Small, Medium, Large
+}
